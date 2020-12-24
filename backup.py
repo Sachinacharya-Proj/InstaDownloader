0e0754375fcfe601c7d3392fff497cd58ad02c6c
Sachin Acharya <Idoncare@sachinhck.com>
2020-12-24 12:24:54 +0545

    Old Script

--- backup.py
+++ backup.py
@@ -1,32 +1,35 @@
-import instaloader
-import os
-import subprocess
+import os, instaloader, subprocess
 
+permissionAsk = input('Wanna a Download Videos to (Y/N)(Default: N)=>> ')
+permissionAsk = permissionAsk.lower()
+
+permissionCode = 0
+
+if permissionAsk == 'y':
+    permissionCode = 1
+elif permissionAsk == 'n':
+    permissionCode = 0
+else:
+    permissionCode = 0
+
+profile_name = input("Enter username: ")
 modelImage = instaloader.Instaloader()
-profile_name = input('Enter Your Username: ')
 
-modelImage.download_profile(profile_name, profile_pic_only= True)
-modelImage.download_profiles(profile_name, profile_pic=True, posts=True, igtv=True, fast_update=True)
-
-# Downloading Images
 modelImage.download_profile(profile_name, profile_pic=True, profile_pic_only=False, fast_update=True)
 
-# READING CONTAING DIRECTORY
 
 dirOpen = os.listdir(profile_name)
-imageDirCount = 0
-videoDirCount = 0
-
 
 for image in dirOpen:
     if image.endswith(".jpg") or image.endswith(".png"):
-        if imageDirCount == 0:
-            os.mkdir(f"{profile_name}/Images")
-            imageDirCount = 2
-        os.system(f"move './{profile_name}/{image}' './{profile_name}/Images/{image}'")
+        pass
     elif image.endswith(".mp4") or image.endswith(".avi"):
-        if videoDirCount == 0:
-            os.mkdir(f"{profile_name}/Videos")
-            videoDirCount = 2
+        if permissionCode == 0:
+            subprocess.run(f'del "{profile_name}\\{image}"', shell=True)
+        else:
+            pass
     else:
         subprocess.run(f'del "{profile_name}\\{image}"', shell=True)
+
+subprocess.run('cls', shell=True)
+print(f'Download Completed and files are stored in Folder {profile_name}')
